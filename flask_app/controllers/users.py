83ae11898f3b3f6027bfbc0019177d34fb561bd5
KobyKhauv <97271646+KobyKhauv@users.noreply.github.com>
2023-02-16 15:49:43 -0800

    Add files via upload

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,105 @@
+from flask import render_template,redirect,session,request, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register',methods=['POST'])
+def register():
+
+    if not User.validate_register(request.form):
+        return redirect('/')
+    data ={ 
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.save(data)
+    session['user_id'] = id
+
+    return redirect('/dashboard')
+
+@app.route('/login',methods=['POST'])
+def login():
+    user = User.get_by_email(request.form)
+    if not user:
+        flash("Invalid Email","login")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid Password","login")
+        return redirect('/')
+    session['user_id'] = user.id
+    return redirect('/dashboard')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("dashboard.html",user=User.get_by_id(data))
+
+@app.route('/phrases')
+def phrases():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("phrases.html",user=User.get_by_id(data))
+
+@app.route('/account')
+def account():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("account.html",user=User.get_by_id(data))
+
+@app.route('/numbers')
+def numbers():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("numbers.html",user=User.get_by_id(data))
+
+@app.route('/colors')
+def colors():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("colors.html",user=User.get_by_id(data))
+
+@app.route('/notes')
+def notes():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("notes.html",user=User.get_by_id(data))
+
+@app.route('/translator')
+def translator():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    data ={
+        'id': session['user_id']
+    }
+    return render_template("translator.html",user=User.get_by_id(data))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
